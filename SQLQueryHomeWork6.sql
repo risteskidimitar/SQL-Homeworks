aea75adced0860f09d879b48f55de5499c814396
risteskidimitar <60626102+risteskidimitar@users.noreply.github.com>
2020-08-19 13:32:54 +0200

    hw6

--- SQLQueryHomeWork6.sql
+++ SQLQueryHomeWork6.sql
@@ -29,4 +29,21 @@ execute dbo.CreateGrade '1', '1', '1', '10', 'NoComment', '2020-08-19'
 
 --Create new procedure called CreateGradeDetail
 --Procedure should add details for specific Grade (new record for new AchievementTypeID, Points, MaxPoints, Date for specific Grade)
---Output from this procedure should be resultset with SUM of GradePoints calculated with formula AchievementPoints/AchievementMaxPoints*ParticipationRate for specific Grade
+--Output from this procedure should be resultset with SUM of GradePoints calculated with formula 
+--AchievementPoints/AchievementMaxPoints*ParticipationRate for specific Grade
+
+create procedure dbo.CreateGradeDetail 
+(@GradeId int, @AchievementTypeID tinyint, @Points tinyint, @MaxPoints tinyint, @Date datetime)
+as
+begin
+insert into dbo.GradeDetails ([GradeID],[AchievementTypeID],[AchievementPoints],[AchievementMaxPoints],[AchievementDate])
+values (@GradeId, @AchievementTypeID, @Points, @MaxPoints, @Date)
+select a.[Name] as AchievementTypeName, sum(gd.AchievementPoints/gd.AchievementMaxPoints*a.ParticipationRate) as SUMofGradePoints
+from dbo.GradeDetails gd
+inner join dbo.AchievementType a on gd.AchievementTypeID = a.ID
+group by a.[Name]
+end
+
+go
+select * from dbo.GradeDetails
+exec dbo.CreateGradeDetail @GradeId = '1', @AchievementTypeID = '1', @Points = '99', @MaxPoints = '100', @Date = '2020-08-19'
